563dc8b7d4dc4fad0320eae097533cdadc0ff93e
Alirezatd80 <alirezatd80@gmail.com>
2026-07-05 22:02:09 +0330

    database configuration and setting

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings , SettingsConfigDict
+
+class  Setting(BaseSettings):
+    DATABASE_URL :str 
+    model_config = SettingsConfigDict(env_file='.env',
+                                      env_file_encoding='utf-8')
+
+setting = Setting() # type: ignore
---
+++ app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker , declarative_base ,Session
+from app.config import setting
+
+engine = create_engine(
+    setting.DATABASE_URL,
+    connect_args={"check_same_thread" : False}
+)
+
+SessionLocal = sessionmaker(autoflush=False,autocommit= False , bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db :Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
